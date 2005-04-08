84bfd368bef46acf861affbacdb66b574ff3c145
iamsure <iamsure@c1a1a65e-67f3-0310-80c4-a658a28374c0>
2005-04-08 19:10:31 +0000

    New file.

---
+++ branches/iamsure/global_includes.php
@@ -0,0 +1,44 @@
+<?php
+// Adodb handles database abstraction
+if (!@include_once ("./backends/adodb/adodb.inc.php"))
+{
+    echo "adodb.inc.php ";
+    echo "cannot be found, and it is required for TKI/BNT to run.";
+    die();
+}
+
+// Adodb handles database abstraction
+if (!@include_once ("./backends/adodb/adodb-perf.inc.php"))
+{
+    echo "adodb-perf.inc.php ";
+    echo "cannot be found, and it is required for TKI/BNT to run.";
+    die();
+}
+
+// XML Schema handler
+if (!@include_once ("./backends/adodb/adodb-xmlschema.inc.php"))
+{
+    echo "adodb-xmlschema.inc.php ";
+    echo "cannot be found, and it is required for TKI/BNT to run.";
+    die();
+}
+
+/*
+// encrypted session handler
+if (!@include_once ("./backends/adodb/session/adodb-cryptsession.php"))
+{
+    echo "adodb-cryptsession.php ";
+    echo "cannot be found, and it is required for TKI/BNT to run.";
+    die();
+}
+
+// compressed session handler
+if (!@include_once ("./backends/adodb/session/adodb-compress-gzip.php"))
+{
+    echo "adodb-compress-gzip.php ";
+    echo "cannot be found, and it is required for TKI/BNT to run.";
+    die();
+}
+*/
+
+?>
