47793d7468dc30e484ba63cc81ef580007bfb522
iamsure <iamsure@c1a1a65e-67f3-0310-80c4-a658a28374c0>
2005-06-11 15:58:33 +0000

    Cleaned up to match coding guidelines.

--- branches/iamsure/about.php
+++ branches/iamsure/about.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once "common.php";
 page_header("About The Dragon Saga");
 $time = (strtotime(date("1971-m-d H:i:s",strtotime("now -".getsetting("gameoffsetseconds",0)." seconds"))))*getsetting("daysperday",4) % strtotime("1971-01-01 00:00:00"); 
@@ -19,129 +19,130 @@ if (!isset($_GET['op']))
     $_GET['op']='';
 }
 
-if ($_GET['op']==""){
-	$order=array("1","2");
-	while (list($key,$val)=each($order)){
-		switch($val){
-		case "2":
-			/* NOTICE
-			 * NOTICE Server admins may put their own information here, please leave the main about body untouched.
-			 * NOTICE
-			 */
-			output("<hr>",true);
-			//output("Put your own information here and uncomment this line by removing the '/' marks.");
-			
-			break;
-		case "1":
-			/* NOTICE
-			 * NOTICE This section may not be modified, please modify the Server Specific section above.
-			 * NOTICE
-			 */
-			output("`@`cThe Dragon Saga - {$logd_version}`c`n`n");
-			//This section may not be modified, please modify the Server Specific section above.
+if ($_GET['op']=="")
+{
+    $order = array("1","2");
+    while (list($key,$val)=each($order))
+    {
+        switch($val)
+        {
+            case "2":
+                // NOTICE
+                // NOTICE Server admins may put their own information here, please leave the main about body untouched.
+                // NOTICE
+                output("<hr>",true);
+                //output("Put your own information here and uncomment this line by removing the '/' marks.");
+                break;
 
-			output("`2The Dragon Saga is an epic open source, web-based, massively multiplayer free roleplaying game.");
-			output("It is collaboratively developed by a diverse group of people around the world, like you.`n`n");
-			output("`@`2You may download the latest version of <a href='http://develop.thedragonsaga.com/' target='_blank'>The Dragon Saga</a>",true);
-			output(" and you may also <a href='http://play.thedragonsaga.com/'>play the latest version of The Dragon Saga</a>.`n`n",true);
-			output("`@`2The Dragon Saga is programmed in PHP with an SQL backend.  It is known to run on Windows and Linux with appropriate
-				setups.  The code is developed under the 
+            case "1":
+	        // NOTICE
+                // NOTICE This section may not be modified, please modify the Server Specific section above.
+                // NOTICE
+                output("`@`cThe Dragon Saga - {$logd_version}`c`n`n");
+                // This section may not be modified, please modify the Server Specific section above.
+
+                output("`2The Dragon Saga is an epic open source, web-based, massively multiplayer free roleplaying game.");
+                output("It is collaboratively developed by a diverse group of people around the world, like you.`n`n");
+                output("`@`2You may download the latest version of <a href='http://develop.thedragonsaga.com/' target='_blank'>The Dragon Saga</a>",true);
+                output(" and you may also <a href='http://play.thedragonsaga.com/'>play the latest version of The Dragon Saga</a>.`n`n",true);
+                output("`@`2The Dragon Saga is programmed in PHP with an SQL backend.  It is known to run on Windows and Linux with appropriate 
+                       setups.  The code is developed under the 
 				<a href=\"http://www.gnu.org/copyleft/gpl.html\">GNU General Public License</a>, which essentially means that the source code to the game, and all derivatives of the game must remain open and",true);
-			output("available upon request.`n`n");
+                output("available upon request.`n`n");
+                output("`@`2Users of the code are free to view and modify the source, but copyright information, and
+                        original text from the about page must be preserved, though they may be added to.");
+                output("`n`nThe majority of the game code came from Legend of the Green Dragon, another similar project, that is now developed 
+                        under the <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.0/\">Creative Commons License.</a>",true);
+                output("`n`nThe original game code, developed by Eric Stevens and JT Traub, with a design by Chris Yarbrough, remains under their copyright.");
 
-			output("`@`2Users of the code are free to view and modify the source, but copyright information, and
-				original text from the about page must be preserved, though they may be added to.");
-
-                        output("`n`nThe majority of the game code came from Legend of the Green Dragon, another similar project, that is now developed under the <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.0/\">Creative Commons License.</a>",true);
-                        output("`n`nThe original game code, developed by Eric Stevens and JT Traub, with a design by Chris Yarbrough, remains under their copyright.");
-
-                        output("`n`nYou may download the latest official version of LoGD at <a href=\"http://sourceforge.net/projects/lotgd\">http://sourceforge.net/projects/lotgd</a>",true);
-                        output("and you can play the version currently in development at http://lotgd.net.");
-                        output("`n`nLegend of the Green Dragon is a remake of and homage to the classic");
-			output("BBS Door game, Legend of the Red Dragon (aka LoRD) by <a href=\"http://www.rtsoft.com/\">Seth Able Robinson</a>.  ",true);
-			output("`n`n`@`2LoRD is now owned by Gameport (<a href='http://www.gameport.com/bbs/lord.html'>http://www.gameport.com/bbs/lord.html</a>), and ",true);
-			output("they retain exclusive rights to the LoRD name and game.  That's why all content in ");
-			output("Legend of the Green Dragon (and by extension, TDS) is new, with only a very few nods to the original game, such ");
-			output("as the buxom barmaid, Violet, and the handsome bard, Seth.`n`n");
-			output("`@`2Although serious effort was made to preserve the original feel of the game, ");
-			output("numerous departures were taken from the original game to enhance playability, and ");
-			output("to adapt it to the web.`n`n");
-
-			output("`@`2We hope you enjoy the game!`@");
-
-			//This section may not be modified, please modify the Server Specific section above.
-			break;
-		}
-	}
-	
-	addnav("Game Setup Info","about.php?op=setup");
-}elseif($_GET['op']=="setup"){
-	addnav("About TDS","about.php");
-	$setup = array(
-		"Game Setup,title",
-		"pvp"=>"Enable Slay Other Players,viewonly",
-		"pvpday"=>"Player Fights per day,viewonly",
-		"pvpimmunity"=>"Days that new players are safe from PvP,viewonly",
-		"pvpminexp"=>"Amount of experience when players become killable in PvP,viewonly",
-		"soap"=>"Clean user posts (filters bad language and splits words over 45 chars long),viewonly",
-		"newplayerstartgold"=>"Amount of gold to start a new character with,viewonly",
-		"New Days,title",
-		"fightsforinterest"=>"Player must have fewer than how many forest fights to earn interest?,viewonly",
-		"maxinterest"=>"Max Interest Rate (%),viewonly",
-		"mininterest"=>"Min Interest Rate (%),viewonly",
-		"daysperday"=>"Game days per calendar day,viewonly",
-		"specialtybonus"=>"Extra daily uses in specialty area,viewonly",
-
-		"Bank settings,title",
-		"borrowperlevel"=>"Max amount player can borrow per level,viewonly",
-		"transferperlevel"=>"Max amount player can transfer per level of recipient,viewonly",
-		"mintransferlev"=>"Minimum level a player has to be before they can transfer gold,viewonly",
-		"transferreceive"=>"Total transfers a player can receive in one play day,viewonly",
-		"maxtransferout"=>"Max amount total a player can transfer to others per level,viewonly",
-		
-		"Bounty,title",
-		"bountymin"=>"Minimum amount per level of target for bounty,viewonly",
-		"bountymax"=>"Maximum amount per level of target for bounty,viewonly",
-		"bountylevel"=>"Minimum player level for being a bounty target,viewonly",
-		"bountyfee"=>"Percentage of bounty kept by Dag Durnick,viewonly",
-		"maxbounties"=>"How many bounties can a person set per day,viewonly",
-
-		"Forest,title",
-		"turns"=>"Forest Fights per day,viewonly",
-		"dropmingold"=>"Forest Creatures always drop at least 1/4 of possible gold,viewonly",
-		"lowslumlevel"=>"Minimum level to allow slumming,viewonly",
-		
-		"Mail Settings,title",
-		"mailsizelimit"=>"Message size limit per message,viewonly",
-		"inboxlimit"=>"Limit # of messages in inbox,viewonly",
-		"oldmail"=>"Automatically delete old messages after (days),viewonly",
-	
-		"Content Expiration,title",
-		"expirecontent"=>"Days to keep comments and news?  (0 for infinite),viewonly",
-		"expiretrashacct"=>"Days to keep accounts that were never logged in to? (0 for infinite),viewonly",
-		"expirenewacct"=>"Days to keep level 1 accounts with no dragon kills? (0 for infinite),viewonly",
-		"expireoldacct"=>"Days to keep all other accounts? (0 for infinite),viewonly",
-		"LOGINTIMEOUT"=>"Seconds of inactivity before auto-logoff,viewonly",
-	
-		"Useful Information,title",
-		"Day Duration: ".round(($dayduration/60/60),0)." hours,viewonly",
-		"Current Server Time: ".date("Y-m-d h:i:s a").",viewonly",
-		"Last new day: ".date("h:i:s a",strtotime("-$realsecssofartoday seconds")).",viewonly",
-		"Current game time: ".getgametime().",viewonly",
-		"Next new day: ".date("h:i:s a",strtotime("+$realsecstotomorrow seconds"))." (".date("H\\h i\\m s\\s",strtotime("1970-01-01 00:00:00 + $realsecstotomorrow seconds"))."),viewonly"
-		);
-	
-	output("`@<h3>Settings for this game</h3>`n`n",true);
-	//output("<table border=1>",true);
-	showform($setup,$settings,true);
-	//output("</table>",true);
-}else{
-
+                output("`n`nYou may download the latest official version of LoGD at <a href=\"http://sourceforge.net/projects/lotgd\">http://sourceforge.net/projects/lotgd</a>",true);
+                output("and you can play the version currently in development at http://lotgd.net.");
+                output("`n`nLegend of the Green Dragon is a remake of and homage to the classic");
+                output("BBS Door game, Legend of the Red Dragon (aka LoRD) by <a href=\"http://www.rtsoft.com/\">Seth Able Robinson</a>.  ",true);
+                output("`n`n`@`2LoRD is now owned by Gameport (<a href='http://www.gameport.com/bbs/lord.html'>http://www.gameport.com/bbs/lord.html</a>), and ",true);
+                output("they retain exclusive rights to the LoRD name and game.  That's why all content in ");
+                output("Legend of the Green Dragon (and by extension, TDS) is new, with only a very few nods to the original game, such ");
+                output("as the buxom barmaid, Violet, and the handsome bard, Seth.`n`n");
+                output("`@`2Although serious effort was made to preserve the original feel of the game, ");
+                output("numerous departures were taken from the original game to enhance playability, and ");
+                output("to adapt it to the web.`n`n");
+                output("`@`2We hope you enjoy the game!`@");
+                break;
+        }
+    }
+    addnav("Game Setup Info","about.php?op=setup");
 }
-if ($session['user']['loggedin']) {
-	addnav("Return to the news","news.php");
-}else{
-	addnav("Login Page","index.php");
+elseif($_GET['op']=="setup")
+{
+    addnav("About TDS","about.php");
+    $setup = array(
+        "Game Setup,title",
+        "pvp"=>"Enable Slay Other Players,viewonly",
+        "pvpday"=>"Player Fights per day,viewonly",
+        "pvpimmunity"=>"Days that new players are safe from PvP,viewonly",
+        "pvpminexp"=>"Amount of experience when players become killable in PvP,viewonly",
+        "soap"=>"Clean user posts (filters bad language and splits words over 45 chars long),viewonly",
+        "newplayerstartgold"=>"Amount of gold to start a new character with,viewonly",
+        "New Days,title",
+        "fightsforinterest"=>"Player must have fewer than how many forest fights to earn interest?,viewonly",
+        "maxinterest"=>"Max Interest Rate (%),viewonly",
+        "mininterest"=>"Min Interest Rate (%),viewonly",
+        "daysperday"=>"Game days per calendar day,viewonly",
+        "specialtybonus"=>"Extra daily uses in specialty area,viewonly",
+
+        "Bank settings,title",
+        "borrowperlevel"=>"Max amount player can borrow per level,viewonly",
+        "transferperlevel"=>"Max amount player can transfer per level of recipient,viewonly",
+        "mintransferlev"=>"Minimum level a player has to be before they can transfer gold,viewonly",
+        "transferreceive"=>"Total transfers a player can receive in one play day,viewonly",
+        "maxtransferout"=>"Max amount total a player can transfer to others per level,viewonly",
+
+        "Bounty,title",
+        "bountymin"=>"Minimum amount per level of target for bounty,viewonly",
+        "bountymax"=>"Maximum amount per level of target for bounty,viewonly",
+        "bountylevel"=>"Minimum player level for being a bounty target,viewonly",
+        "bountyfee"=>"Percentage of bounty kept by Dag Durnick,viewonly",
+        "maxbounties"=>"How many bounties can a person set per day,viewonly",
+
+        "Forest,title",
+        "turns"=>"Forest Fights per day,viewonly",
+        "dropmingold"=>"Forest Creatures always drop at least 1/4 of possible gold,viewonly",
+        "lowslumlevel"=>"Minimum level to allow slumming,viewonly",
+
+        "Mail Settings,title",
+        "mailsizelimit"=>"Message size limit per message,viewonly",
+        "inboxlimit"=>"Limit # of messages in inbox,viewonly",
+        "oldmail"=>"Automatically delete old messages after (days),viewonly",
+
+        "Content Expiration,title",
+        "expirecontent"=>"Days to keep comments and news?  (0 for infinite),viewonly",
+        "expiretrashacct"=>"Days to keep accounts that were never logged in to? (0 for infinite),viewonly",
+        "expirenewacct"=>"Days to keep level 1 accounts with no dragon kills? (0 for infinite),viewonly",
+        "expireoldacct"=>"Days to keep all other accounts? (0 for infinite),viewonly",
+        "LOGINTIMEOUT"=>"Seconds of inactivity before auto-logoff,viewonly",
+
+        "Useful Information,title",
+        "Day Duration: ".round(($dayduration/60/60),0)." hours,viewonly",
+        "Current Server Time: ".date("Y-m-d h:i:s a").",viewonly",
+        "Last new day: ".date("h:i:s a",strtotime("-$realsecssofartoday seconds")).",viewonly",
+        "Current game time: ".getgametime().",viewonly",
+        "Next new day: ".date("h:i:s a",strtotime("+$realsecstotomorrow seconds"))." (".date("H\\h i\\m s\\s",strtotime("1970-01-01 00:00:00 + $realsecstotomorrow seconds"))."),viewonly"
+    );
+
+    output("`@<h3>Settings for this game</h3>`n`n",true);
+    //output("<table border=1>",true);
+    showform($setup,$settings,true);
+    //output("</table>",true);
 }
+
+if ($session['user']['loggedin'])
+{
+    addnav("Return to the news","news.php");
+}
+else
+{
+    addnav("Login Page","index.php");
+}
+
 page_footer();
 ?>
